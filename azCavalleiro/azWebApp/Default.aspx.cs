3ce44e84df36bf72b5ad99b258118d1708bc0bca
carloscavalleiro <carloscavalleiro@hotmail.com>
2017-06-15 22:12:47 -0300

    Ajuste da pagina inicial

--- azCavalleiro/azWebApp/Default.aspx.cs
+++ azCavalleiro/azWebApp/Default.aspx.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
 
 namespace azWebApp
 {
-    public partial class _Default : Page
+    public partial class azWebApp : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Label1.Text = "Bemvindo ao site azCavalleiro ";
+            Label2.Text = "Versão da publicação " + ConfigurationManager.AppSettings["lcVersion"];
+            Label3.Text = "Versão do site       " + ConfigurationManager.AppSettings["azVersion"];
         }
     }
 }
