4c35b387615cfef3518afe7f187afee71f465b74
carloscavalleiro <carloscavalleiro@hotmail.com>
2017-06-15 21:57:21 -0300

    Inclusão do form azWebApp

---
+++ azCavalleiro/azWebApp/azWebApp.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace azWebApp
+{
+    public partial class azWebApp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "Bemvindo ao site azCavalleiro ";
+            Label2.Text = "Versão da publicação " + ConfigurationManager.AppSettings["lcVersion"];
+            Label3.Text = "Versão do site       " + ConfigurationManager.AppSettings["azVersion"];
+        }
+    }
+}
